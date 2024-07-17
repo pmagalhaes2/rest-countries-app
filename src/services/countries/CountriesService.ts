ca10ae2d8c37d8732b33a0eca395fe98cddc51d1
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-07-17 14:17:47 -0300

    fix: ajuste nomenclatura dos tipos

--- src/services/countries/CountriesService.ts
+++ src/services/countries/CountriesService.ts
@@ -1,17 +1,17 @@
 import { ApiService } from "../ApiService";
-import { Country } from "../types/Country";
+import { ICountry } from "../types/Country";
 
 const apiService = new ApiService();
 
 class CountriesService {
-  getCountries(): Promise<Country[]> {
+  getCountries(): Promise<ICountry[]> {
     return apiService.get({
       endpoint: `/all`,
       config: { "Content-Type": "application/json" },
     });
   }
 
-  getBordersByCode(codes: string[]): Promise<Country[]> {
+  getBordersByCode(codes: string[]): Promise<ICountry[]> {
     return apiService.get({
       endpoint: `/alpha?codes=${codes}`,
       config: { "Content-Type": "application/json" },

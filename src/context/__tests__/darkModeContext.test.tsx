e9f583dc0cd4a3a2ebd9851f10576f468fdb46ae
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-07-24 16:07:45 -0300

    feat: criação de teste para contexto

---
+++ src/context/__tests__/darkModeContext.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { DarkModeContextProvider } from "../darkModeContext";
+import { act } from "react";
+import { Header } from "../../components/Header/Header";
+
+describe("Dark mode context", () => {
+  beforeEach(async () => {
+    await act(async () => {
+      render(
+        <DarkModeContextProvider>
+          <Header title={"Test"} />
+        </DarkModeContextProvider>
+      );
+    });
+  });
+  it("deve renderizar o provider com um componente filho", async () => {
+    const header = screen.getByRole("banner");
+
+    expect(header).toBeInTheDocument();
+  });
+
+  it("deve alterar o background quando o botão Dark Mode for acionado", async () => {
+    const header = screen.getByRole("banner");
+
+    const button = screen.getByRole("button");
+
+    await act(async () => {
+      fireEvent.click(button);
+    });
+
+    expect(header).toHaveStyle("background-color: var(--dark-mode-elements)");
+  });
+});

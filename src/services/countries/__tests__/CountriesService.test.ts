16ac26b9cb0c5dfc3e07fab8798971a827218326
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-07-29 18:55:07 -0300

    chore: criação de mais asserções nos testes do serviço

--- src/services/countries/__tests__/CountriesService.test.ts
+++ src/services/countries/__tests__/CountriesService.test.ts
@@ -1,11 +1,10 @@
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
-import {
-  countriesService,
-} from "../CountriesService";
+import { countriesService } from "../CountriesService";
 import { MOCK_COUNTRIES } from "../mocks/MockCountries";
 import { MOCK_BORDERS } from "../mocks/MockBorders";
 import { MOCK_COUNTRY_DETAILS } from "../mocks/MockCountryDetails";
+import { ICountry } from "../../../interfaces/Country";
 
 const mock = new MockAdapter(axios);
 
@@ -16,6 +15,13 @@ describe("Countries Service", () => {
     const countries = await countriesService.getCountries();
 
     expect(countries.data.length).toBeGreaterThan(0);
+
+    MOCK_COUNTRIES.data.forEach((mockCountry) => {
+      const found = countries.data.some(
+        (country: ICountry) => country.name.common === mockCountry.name.common
+      );
+      expect(found).toBe(true);
+    });
   });
 
   it("deve retornar as informações dos países vizinhos de acordo com os códigos recebidos", async () => {
@@ -26,6 +32,13 @@ describe("Countries Service", () => {
     const borders = await countriesService.getBordersByCode(["CAN", "MEX"]);
 
     expect(borders.data.length).toBe(2);
+
+    MOCK_BORDERS.data.forEach((mockBorder) => {
+      const found = borders.data.some(
+        (border: ICountry) => border.name.common === mockBorder.name.common
+      );
+      expect(found).toBe(true);
+    });
   });
 
   it("deve retornar a informação do país de acordo com o nome recebido", async () => {
@@ -34,5 +47,13 @@ describe("Countries Service", () => {
     const country = await countriesService.getCountryByName("Brazil");
 
     expect(country.data.length).toBe(1);
+
+    MOCK_COUNTRY_DETAILS.data.forEach((mockCountryDetails) => {
+      const found = country.data.every(
+        (country: ICountry) =>
+          country.name.common === mockCountryDetails.name.common
+      );
+      expect(found).toBe(true);
+    });
   });
 });
